309b162dd08b9fef6bbd217094c8469a323b0564
Richard Bruskiewich <richard@starinformatics.com>
2018-08-27 13:32:24 -0700

    Baby steps exploring the API's

--- server/swagger_server/controllers/query_controller.py
+++ server/swagger_server/controllers/query_controller.py
@@ -4,7 +4,7 @@ import six
 from swagger_server.models.query import Query  # noqa: E501
 from swagger_server.models.response import Response  # noqa: E501
 from swagger_server import util
-
+from swagger_server.services.kba import KBA
 
 def query(body):  # noqa: E501
     """Query using a predefined query type
@@ -18,4 +18,4 @@ def query(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Query.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return KBA.processQuery(body)
