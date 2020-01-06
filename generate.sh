369ccbf54ded852e5d1f58d3ef87eed62b42aefe
Richard Bruskiewich <richard@starinformatics.com>
2020-01-06 13:19:48 -0800

    Cleaning up generator.sh script

--- generate.sh
+++ generate.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
-# This script downloads openapi-generator-cli.jar from https://github.com/OpenAPITools/openapi-generator
-# into a hidden directory and then uses it to generate server and client Python project stubs
+# This script configures the openapi-generator-cli from https://raw.githubusercontent.com/OpenAPITools/openapi-generator
+# then uses it to generate server and client Python project stubs
 
 #############################
 ###       Constants       ###
 #############################
 
-# This is a whitespace (space or tab) delimited list of lines that will be added to .openapi-generator-ignore prior to code generation, if they are not already present.
+# This is a whitespace (space or tab) delimited list of lines that will be added to
+# .openapi-generator-ignore prior to code generation, if they are not already present.
 IGNORE_LIST=""
 
-# This is where we will download openapi-generator-cli.jar from latest release 4.*.* version
-OPENAPI_GENERATOR_JAR_LOCATION="http://central.maven.org/maven2/org/openapitools/openapi-generator/4.2.2/openapi-generator-cli-4.2.2.jar -O .openapi-generator/openapi-generator-cli.jar"
+# This is where we will download openapi-generator-cli from the latest release
+OPENAPI_GENERATOR_CLI="openapi-generator-cli"
+OPENAPI_GENERATOR_CLI_PATH="/usr/local/bin/"+${OPENAPI_GENERATOR_CLI}
 
-# Here we define the names of the directories that the server and client projects will be generated into
+# Here we define the default values of the code generation parameters
+# guiding the generation of the server and client projects (these may be overridden in the invoking environment)
 PROJECT_NAME="kba-reasoner"
 
-SERVER_OUTPUT_DIR="server"
-SERVER_PACKAGE_NAME="reasoner_server"
-SERVER_PACKAGE_VERSION="0.9.2"
-
+CLIENT_SPECIFICATION_FILE_PATH=./kba_api/beacon-aggregator-api.yaml
 CLIENT_OUTPUT_DIR="client"
 CLIENT_PACKAGE_NAME="kba_client"
 CLIENT_PACKAGE_VERSION="1.1.1"
+CLIENT_PACKAGE_URL="https://github.com/NCATS-Tangerine/kba-reasoner/tree/master/client"
+
+SERVER_SPECIFICATION_FILE_PATH=./reasoner_api/API/TranslatorReasonersAPI.yaml
+SERVER_OUTPUT_DIR="server"
+SERVER_PACKAGE_NAME="reasoner_server"
+SERVER_PACKAGE_VERSION="0.9.2"
+SERVER_PACKAGE_URL="https://github.com/NCATS-Tangerine/kba-reasoner/tree/master/server"
+SERVER_PORT=8080
 
 #############################
 ###        Methods        ###
@@ -29,15 +37,16 @@ CLIENT_PACKAGE_VERSION="1.1.1"
 
 # Called when there is an error with how the script is being used
 usage() {
-echo "usage: $(basename "$0") <command> <specification> -- uses OpenAPI Generator to generate a python project stub
+echo "usage: $(basename "$0") <command> [<specification>[ -- uses OpenAPI Generator to generate a python project stub
 
 	command:
+		validate	to validate the default or an (optional) API specification provided on the command line
 		server		to generate a server
 		client		to generate a client
-		clean		to delete .openapi-generator/openapi-generator-cli.jar
+		clean		to delete openapi-generator-cli script
 
 	specification:
-		path to a json or yaml specification file to be used for generating the server or client project stub"
+		path to an optional json or yaml specification file to be used for generating the server or client project stub"
 	exit 1
 }
 
@@ -47,8 +56,9 @@ ensureValidIgnoreFile() {
 	IGNORE_FILE="$DIRECTORY/.openapi-generator-ignore"
 
 	if [ ! -f "$IGNORE_FILE" ]; then
-		# Swagger automatically creates an empty .openapi-generator-ignore file, so this must mean that the project hasn't ever been generated before.
-		# The ignore functionality applies not just to overwriting preexisting files, but also creating files. So we don't want an ignore file active in this case.
+		# Swagger automatically creates an empty .openapi-generator-ignore file, so this must mean that the project
+		# hasn't ever been generated before. The ignore functionality applies not just to overwriting preexisting
+		# files, but also creating files. So we don't want an ignore file active in this case.
 		return
 	fi
 
@@ -76,13 +86,15 @@ if [ -z "$1" ]; then
 else
 	COMMAND="$1"
 
-	if   [ "$COMMAND" = client ]; then
+	if [ "$COMMAND" = validate ]; then
+		:
+	elif [ "$COMMAND" = client ]; then
 		:
 	elif [ "$COMMAND" = server ]; then
 		:
 	elif [ "$COMMAND" = clean ]; then
 		# redirect output to /dev/null to prevent it from printing
-		rm .openapi-generator/openapi-generator-cli.jar 2> /dev/null || echo "There is nothing to clean"
+		rm ${OPENAPI_GENERATOR_CLI_PATH} 2> /dev/null || echo "There is nothing to clean"
 		exit 0
 	else
 		echo "Invalid command\n"
@@ -90,17 +102,9 @@ else
 	fi
 fi
 
-# Get the specification file
-if [ -z "$2" ]; then
-		if [ "$COMMAND" = client ]; then
-			SPECIFICATION_FILE_PATH=kba_api/beacon-aggregator-api.yaml
-	    elif [ "$COMMAND" = server ]; then
-			SPECIFICATION_FILE_PATH=reasoner_api/API/TranslatorReasonersAPI.yaml
-		else
-			echo "No default for unknown specification file\n"
-			usage
-		fi
-else
+# Get a user specified specification file, if provided
+if [ -n "$2" ]; then
+
 	SPECIFICATION_FILE_PATH="$2"
 
 	if [ "${SPECIFICATION_FILE_PATH#*.yaml}" != $SPECIFICATION_FILE_PATH ]; then
@@ -111,67 +115,73 @@ else
 		echo "Invalid specification file\n"
 		usage
 	fi
+
+	# Command line overrides
+    if [ "$COMMAND" = client ]; then
+        CLIENT_SPECIFICATION_FILE_PATH=$SPECIFICATION_FILE_PATH
+    elif [ "$COMMAND" = server ]; then
+        SERVER_SPECIFICATION_FILE_PATH=$SPECIFICATION_FILE_PATH
+    else
+        echo "No default for unknown specification file\n"
+        usage
+    fi
 fi
 
 # Attempt to download openapi-generator-cli.jar if it doesn't already exist
-if [ -f ".openapi-generator/openapi-generator-cli.jar" ]; then
+if [ -f ${OPENAPI_GENERATOR_CLI_PATH} ]; then
 	:
 else
-	mkdir -p .openapi-generator
-
-	# wget creates a file whether or not it's able to download it, so if there's a download error we want to delete the file that was created.
-	wget $OPENAPI_GENERATOR_JAR_LOCATION || rm -f .openapi-generator/openapi-generator-cli.jar
-
-	# If download failed then exit
-	if [ ! -f .openapi-generator/openapi-generator-cli.jar ]; then
-		echo "Failed to download openapi-generator-cli.jar"
-		exit 1
-	fi
+	echo "(Re-)installing OpenAPI Generator Tool (may need to be 'sudo' to succeed)"
+	curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh \
+		> ${OPENAPI_GENERATOR_CLI_PATH}
+	chmod u+x ${OPENAPI_GENERATOR_CLI_PATH}
+	${OPENAPI_GENERATOR_CLI} version
 fi
 
 # Use openapi-generator-cli.jar to generate the server and client stub
 if [ "$COMMAND" = validate ]; then
 
-	java -jar .openapi-generator/openapi-generator-cli.jar generate -i $SPECIFICATION_FILE_PATH -g python -o $CLIENT_OUTPUT_DIR \
-	          -D packageName=$CLIENT_PACKAGE_NAME -D projectName=$PROJECT_NAME -D packageVersion=$SERVER_PACKAGE_VERSION
+    if [ -z "$2" ]; then
+
+		${OPENAPI_GENERATOR_CLI} validate --input-spec=${CLIENT_SPECIFICATION_FILE_PATH}
+		${OPENAPI_GENERATOR_CLI} validate --input-spec=${SERVER_SPECIFICATION_FILE_PATH}
+    else
+		${OPENAPI_GENERATOR_CLI} validate --input-spec=$2
+    fi
 
 	exit 0
 
 elif [ "$COMMAND" = client ]; then
-	ensureValidIgnoreFile "$CLIENT_OUTPUT_DIR"
 
-openapi-generator generate  --input-spec=kba_api/beacon-aggregator-api.yaml \
-                    --model-package=model \
-                    --output=client \
-                    --generator-name=python \
-                    --additional-properties="\
---packageName=client.kba_client,\
---projectName=kba-reasoner,\
-—-packageVersion=\"1.1.1\",\
---packageUrl=https://github.com/NCATS-Tangerine/kba-reasoner/tree/master/client"
+	ensureValidIgnoreFile ${CLIENT_OUTPUT_DIR}
 
-	java -jar .openapi-generator/openapi-generator-cli.jar generate -i $SPECIFICATION_FILE_PATH -g python -o $CLIENT_OUTPUT_DIR \
-	          -D packageName=$CLIENT_PACKAGE_NAME -D projectName=$PROJECT_NAME -D packageVersion=$SERVER_PACKAGE_VERSION
+	${OPENAPI_GENERATOR_CLI} generate --input-spec=${CLIENT_SPECIFICATION_FILE_PATH} \
+	                --model-package=model \
+	                --output=${CLIENT_OUTPUT_DIR} \
+	                --generator-name=python \
+	                --additional-properties="\
+	--projectName=\""+${PROJECT_NAME}+"\",\
+	--packageName=\""+${CLIENT_PACKAGE_NAME}+"\",\
+	—-packageVersion=\""+${CLIENT_PACKAGE_VERSION}+"\",\
+	--packageUrl=\""+${CLIENT_PACKAGE_URL}+"\""
 
 	exit 0
 
 elif [ "$COMMAND" = server ]; then
-	ensureValidIgnoreFile "$SERVER_OUTPUT_DIR"
 
-	openapi-generator generate --input-spec=reasoner_api/API/TranslatorReasonersAPI.yaml \
-                    --model-package=model \
-                    --output=server \
-                    --generator-name=python-flask \
-                    --additional-properties="\
---packageName=server.reasoner_server,\
---projectName=kba-reasoner,\
-—-packageVersion=\"0.9.2\",\
---packageUrl=https://github.com/NCATS-Tangerine/kba-reasoner/tree/master/server,\
---serverPort=8080"
+	ensureValidIgnoreFile ${SERVER_OUTPUT_DIR}
 
-	java -jar .openapi-generator/openapi-generator-cli.jar generate -i $SPECIFICATION_FILE_PATH -g python-flask \
-	          -o $SERVER_OUTPUT_DIR  -D packageName=$SERVER_PACKAGE_NAME -D projectName=$PROJECT_NAME
-	          -D packageVersion=$CLIENT_PACKAGE_VERSION
+	${OPENAPI_GENERATOR_CLI} generate --input-spec=${SERVER_SPECIFICATION_FILE_PATH} \
+	                --model-package=model \
+	                --output=${SERVER_OUTPUT_DIR} \
+	                --generator-name=python-flask \
+	                --additional-properties="\
+	--projectName=\""+${PROJECT_NAME}+"\",\
+	--packageName=\""+${SERVER_PACKAGE_NAME}+"\",\
+	—-packageVersion=\""+${SERVER_PACKAGE_VERSION}+"\",\
+	--packageUrl=\""+${SERVER_PACKAGE_URL}+"\",\
+	--serverPort=\""+${SERVER_PORT}+"\""
+
 	exit 0
 
 else
